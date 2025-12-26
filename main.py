e3e1636a2948960e2daf909865f40554562f15e0
santiago <sansantitiagoago@gmail.com>
2025-12-25 23:43:19 -0300

    errei e precisarei recomeçar
    não verifiquei o arquivo de início do primeiro for e ele pegou da metade da última sessão. talvez possa fazer uma linha alternativa para comparativo, mas depois.

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from functions import *
 from modelos import *
 
 
-model = "openvoice_v1"
+model = "openvoice_v2"
 print(f"\nTrying model: {model}")
 threading.Thread(target=stop_command, daemon=True).start()
 
 file = 'outputs/openvoice_v1.2025-12-22/conversion_002.wav'
-for i in range(98):
+for i in range(86):
     new_file = conversion_models(file, file, model, play=True)
     file = new_file
 
